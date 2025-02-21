6791267a1f4dcac3272b80ab306059d98f435ec8
M-UsmanNasirJS <usmanbinnasir2024@gmail.com>
2025-02-21 07:40:59 +0500

    Products Api

---
+++ app.js
@@ -0,0 +1,39 @@
+console.log("Prodocts Api");
+
+const div = document.querySelector(".container")
+
+
+fetch(`https://dummyjson.com/products`)
+
+.then(res => res.json())
+
+.then(res => {
+    console.log(res.products);
+    res.products.map((item) => {
+        div.innerHTML += `
+
+        <div class="alone-card">
+
+        <img src="${item.thumbnail}" width="60%">
+        <h1>Title : ${item.title}</h1>
+        <h3 class="bg-green">Price : $${item.price}</h3>
+        <p>${item.description.slice(0, 30)}<b>......</b></p>
+
+        <button class="seeMore" onclick="showMore(${item.id})">See More...<button>
+ 
+        </div>
+        `
+    })
+})
+
+.catch(err => {
+    console.log(err);
+    
+})
+
+const showMore = id => {
+    console.log(id);
+    localStorage.setItem("id", id)         
+    window.location = "OneProduct.html"
+}
+
